e3906d9b50c61d8f164fa296871bdefc00efade5
Federico Giuliana <federico.giuliana97@gmail.com>
2020-11-20 19:19:08 +0100

    cancella notebook

---
+++ Jupyter/CancellaJupyter.py
@@ -0,0 +1,45 @@
+from os import path
+from kubernetes import client, config
+
+DEPLOYMENT_NAME = "jupyter-deployment"
+SERVICE_NAME = "jupyter-service"
+INGRESS_NAME = "jupyter-ingress"
+
+ 
+def delete_deployment(api_instance):
+    api_response = api_instance.delete_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(propagation_policy='Foreground',grace_period_seconds=5))
+    print("Deployment deleted. status='%s'" % str(api_response.status))
+
+def delete_service(api_instance):
+    api_response = api_instance.delete_namespaced_service(
+        name=SERVICE_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(propagation_policy='Foreground',grace_period_seconds=5))
+    print("Service deleted. status='%s'" % str(api_response.status))
+
+def delete_ingress(api_instance):
+    api_response = api_instance.delete_namespaced_ingress(
+        name=INGRESS_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(propagation_policy='Foreground',grace_period_seconds=5))
+    print("Ingress deleted. status='%s'" % str(api_response.status))
+
+
+def main():
+
+    config.load_kube_config()
+    apps_v1_api = client.AppsV1Api()
+    core_v1_api = client.CoreV1Api()
+    networking_v1_beta1_api = client.NetworkingV1beta1Api()
+
+    delete_deployment(apps_v1_api)
+    delete_service(core_v1_api)
+    delete_ingress(networking_v1_beta1_api)
+
+
+if __name__ == '__main__':
+    main()
+
